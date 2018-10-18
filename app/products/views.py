17ff3ac417c730036d32573e72f367da0c0fdb24
Sanya Kenneth <sanyakenneth@gmail.com>
2018-10-18 19:17:27 +0300

    Add custom error handlers for the API

--- app/products/views.py
+++ app/products/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint,request,jsonify,abort,json
+from flask import Blueprint,request,jsonify,abort,json,make_response
 from .models import Product
+
 
 # create product blueprint
 # blueprint will handle all product routes for the app
@@ -60,6 +61,32 @@ def get_product(product_id):
 
     abort(404)
        
+
+#Custom error handlers
+@product.app_errorhandler(404)
+def not_found(error):
+    """Function takes in HTTP error 404 and returns custom HTTP error 404 message """
+    return make_response(jsonify({'Error':':( Oops Nothing found'}),404)
+
+@product.app_errorhandler(405)
+def method_not_allowed(error):
+    """Function takes in HTTP error 405 and returns custom HTTP error 405 message """
+    return make_response(jsonify({'Error':':( Oops Your trying to use a wrong HTTP Method'}),405)
+
+@product.app_errorhandler(400)
+def bad_request(error):
+    """Function takes in HTTP error 400 and returns custom HTTP error 400 message """
+    return make_response(jsonify({'Error':':( BAD REQUEST'}),400)
+
+@product.app_errorhandler(401)    
+def unauthorised(error):
+    """Function takes in HTTP error 401 and returns custom HTTP error 401 message """
+    return make_response(jsonify({'Error':'You are not allowed to access this resource'}),401)
+
+@product.app_errorhandler(500)
+def internal_server_error(error):
+    """Function takes in HTTP error 500 and returns custom HTTP error 500 message """
+    return make_response(jsonify({'Error':'Server run into some error'}))
 
     
 
