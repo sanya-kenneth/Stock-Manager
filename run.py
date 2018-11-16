e355079700011778da6a0e4beb7f33957b8e15a1
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-17 01:46:30 +0300

    edit run file

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app
+from app.auth.database import db_handler
 
 # App entry point
-app = create_app("Development")
+app = create_app("Production")
+db_handler().create_tables()
   
 if __name__ == '__main__':
     app.run()
