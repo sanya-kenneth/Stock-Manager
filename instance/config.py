5f668d5b38b7500a149393bee5a283d83018530a
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-17 04:32:33 +0300

    add heroku database url

--- instance/config.py
+++ instance/config.py
@@ -25,6 +25,8 @@ class ProductionConfig(BaseConfig):
     """Production configuraion. Loads Production configuration data when the app is in the Production environment"""
     DEBUG = False
     TESTING = False
+    ENV = 'Production'
+    DATABASE_URI = 'postgres://nuqulvckxqqpdw:b3c27cd42143a3b75bac9bff94b673c914f5009bc22b9bb3ecd694ec7c924b7a@ec2-50-19-249-121.compute-1.amazonaws.com:5432/d8a8v4ger4cv7r'
 
 app_config = {
             "Development":DevelopmentConfig,
