af2ce7dce0ce44f34909c2975a59e4c8ae51948d
Sanya Kenneth <sanyakenneth@gmail.com>
2018-11-18 14:22:39 +0300

    remove add user

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from app import create_app
 from app.auth.database import Database
-from werkzeug.security import generate_password_hash
+
 # App entry point
 app = create_app("Production")
 db = Database(app.config['DATABASE_URI'])
 db.create_tables()
-db.add_user('ben','ben@gmail.com',generate_password_hash('ben'),True)
   
 if __name__ == '__main__':
     app.run()
